a75764becc3b2bbd4b09d96d9087d84a8a496ad5
Mahno4ina <patoraa4@gmail.com>
2023-03-04 16:44:16 +0200

    all

--- index.js
+++ index.js
@@ -1,2 +1,28 @@
-var name= prompt('What is your name?','');
-alert('Hello '+name+' !');
+ var name = prompt("Enter your name:");
+
+ var surname = prompt("Enter your surname");
+
+ var ageNumber = prompt("Enter your age:");
+
+ var city = prompt("Enter your city:");
+
+ var age = Number(ageNumber);
+
+
+ var user = {
+   name,
+   surname,
+   age,
+   city,
+ };
+
+
+ var canShowData = confirm("Do you allow your data to be shown?");
+
+ canShowData
+
+   ? console.log
+
+   (`Username ${user.name}, Surname ${user.surname}, Age ${user.age}, City ${user.city}`)
+
+   : alert("The user has blocked the display of data");
