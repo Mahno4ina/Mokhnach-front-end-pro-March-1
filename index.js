d7bd2e033dc417fb1c8a8c6dbfe9095269bb9f69
Daniel <118555923+Mahno4ina@users.noreply.github.com>
2023-03-05 14:05:46 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
- var name = prompt("Enter your name:");
+ let name = prompt("Enter your name:");
 
- var surname = prompt("Enter your surname");
+ let surname = prompt("Enter your surname");
 
- var ageNumber = prompt("Enter your age:");
+ let ageNumber = prompt("Enter your age:");
 
- var city = prompt("Enter your city:");
+ let city = prompt("Enter your city:");
 
- var age = Number(ageNumber);
+ let age = Number(ageNumber);
 
 
- var user = {
+ let user = {
    name,
    surname,
    age,
@@ -17,7 +17,7 @@
  };
 
 
- var canShowData = confirm("Do you allow your data to be shown?");
+ let canShowData = confirm("Do you allow your data to be shown?");
 
  canShowData
 
