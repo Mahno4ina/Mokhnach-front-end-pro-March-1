ba60c3dce9656916019447b27835f7f9ad348b90
Mahno4ina <patoraa4@gmail.com>
2023-03-21 20:20:42 +0200

    1

--- second.js
+++ second.js
@@ -1,63 +1,16 @@
-const arr = [
-  16, -37, 54, -4, 72, -56, 47, 4, -16, 25, -37, 46, 4, -51, 27, -63, 4, -54,
-  76, -4, 12, -35, 4, 47,
-];
+let phoneNumbers = []; 
+let totalBalance = 0; 
 
-let sumPos = 0;
-let countPos = 0;
-let min = arr[0];
-let minIndex = 0;
-let max = arr[0];
-let maxIndex = 0;
-let countNeg = 0;
-let countOdd = 0;
-let countEven = 0;
-let sumEven = 0;
-let sumOdd = 0;
-let productPos = 1;
-
-for (let i = 0; i < arr.length; i++) {
-  if (arr[i] > 0) {
-    sumPos += arr[i];
-    productPos *= arr[i];
-    countPos++;
-    if (arr[i] % 2 === 0) {
-      countEven++;
-      sumEven += arr[i];
-    } else {
-      countOdd++;
-      sumOdd += arr[i];
-    }
-  } else {
-    countNeg++;
+for (let i = 0; i < users.length; i++) {
+  let user = users[i];
+  let balance = parseFloat(user.balance.replace(/[$,]/g, '')); 
+  if (balance > 2000) {
+    phoneNumbers.push(user.phone);
   }
-  if (arr[i] < min) {
-    min = arr[i];
-    minIndex = i;
-  }
-  if (arr[i] > max) {
-    max = arr[i];
-    maxIndex = i;
-  }
+  totalBalance += balance;
 }
 
-console.log(`Sum of positive elements: ${sumPos}`);
-console.log(`Count of positive elements: ${countPos}`);
-console.log(`Min element: ${min}`);
-console.log(`Index of min element: ${minIndex}`);
-console.log(`Max element: ${max}`);
-console.log(`Index of max element: ${maxIndex}`);
-console.log(`Count of negative elements: ${countNeg}`);
-console.log(`Count of odd positive elements: ${countOdd}`);
-console.log(`Count of even positive elements: ${countEven}`);
-console.log(`Sum of even positive elements: ${sumEven}`);
-console.log(`Sum of odd positive elements: ${sumOdd}`);
-console.log(`Product of positive elements: ${productPos}`);
+console.log("Телефонні номери користувачів з балансом більше 2000 доларів:");
+console.log(phoneNumbers);
 
-for (let i = 0; i < arr.length; i++) {
-  if (arr[i] !== max) {
-    arr[i] = 0;
-  }
-}
-
-console.log(`Array after zeroing all elements except for the max: ${arr}`);
+console.log("Загальна сума балансів користувачів: " + totalBalance);
