bf578ebb9a4be1b031c107324adb4decbcd7146b
Mahno4ina <patoraa4@gmail.com>
2023-03-25 11:04:59 +0200

    1

--- second.js
+++ second.js
@@ -1,16 +1,40 @@
-let phoneNumbers = []; 
-let totalBalance = 0; 
-
-for (let i = 0; i < users.length; i++) {
-  let user = users[i];
-  let balance = parseFloat(user.balance.replace(/[$,]/g, '')); 
-  if (balance > 2000) {
-    phoneNumbers.push(user.phone);
+function removeElement(array, item) {
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === item) {
+      array.splice(i, 1);
+      i--;
+    }
   }
-  totalBalance += balance;
+  return array;
+}
+function generateKey(length, characters) {
+  let result = "";
+  for (let i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * characters.length));
+  }
+  return result;
+}
+function removeChars(str, chars) {
+  let result = "";
+  for (let i = 0; i < str.length; i++) {
+    if (chars.indexOf(str[i]) === -1) {
+      result += str[i];
+    }
+  }
+  return result;
+}
+function sum(num) {
+  let total = num;
+  function add(nextNum) {
+    total += nextNum;
+    return add;
+  }
+  add.toString = function () {
+    return total;
+  };
+  return add;
 }
 
-console.log("Телефонні номери користувачів з балансом більше 2000 доларів:");
-console.log(phoneNumbers);
-
-console.log("Загальна сума балансів користувачів: " + totalBalance);
+console.log(sum(3));
+console.log(sum(5)(3));
+console.log(sum(20)(5)(3));
