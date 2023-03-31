f4f91f11b0b2ca35d587a7dce66b3ff255f47b56
Daniel <118555923+Mahno4ina@users.noreply.github.com>
2023-03-31 13:32:54 +0300

    Update second.js

--- second.js
+++ second.js
@@ -1,40 +1,47 @@
 function removeElement(array, item) {
-  for (let i = 0; i < array.length; i++) {
-    if (array[i] === item) {
-      array.splice(i, 1);
-      i--;
-    }
+  const index = array.indexOf(item);
+  if (index !== -1) {
+    array.splice(index, 1);
   }
   return array;
 }
+
+const array = [1, 2, 3, 4, 5, 6, 7];
+removeElement(array, 3);
+console.log(array);
+
 function generateKey(length, characters) {
   let result = "";
+  const charactersLength = characters.length;
   for (let i = 0; i < length; i++) {
-    result += characters.charAt(Math.floor(Math.random() * characters.length));
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
   }
   return result;
-}
-function removeChars(str, chars) {
-  let result = "";
-  for (let i = 0; i < str.length; i++) {
-    if (chars.indexOf(str[i]) === -1) {
-      result += str[i];
-    }
-  }
-  return result;
-}
-function sum(num) {
-  let total = num;
-  function add(nextNum) {
-    total += nextNum;
-    return add;
-  }
-  add.toString = function () {
-    return total;
-  };
-  return add;
 }
 
+const characters = "abcdefghijklmnopqrstuvwxyz0123456789";
+const key = generateKey(10, characters);
+console.log(key);
+
+function removeChars(str, charsToRemove) {
+  const regex = new RegExp(`[${charsToRemove.join("")}]`, "g");
+  return str.replace(regex, "");
+}
+
+const str = " hello world";
+const charsToRemove = ["l", "d"];
+const newStr = removeChars(str, charsToRemove);
+console.log(newStr);
+
+function createSum() {
+  let sum = 0;
+  return function (num) {
+    sum += num;
+    return sum;
+  };
+}
+
+const sum = createSum();
 console.log(sum(3));
-console.log(sum(5)(3));
-console.log(sum(20)(5)(3));
+console.log(sum(5));
+console.log(sum(20));
