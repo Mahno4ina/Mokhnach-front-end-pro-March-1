700ac6c342c024c10e3c3b10a004a38c6f3ff394
Mahno4ina <patoraa4@gmail.com>
2023-04-11 22:56:39 +0300

    1

--- second.js
+++ second.js
@@ -1,40 +1,76 @@
-function removeElement(array, item) {
-  for (let i = 0; i < array.length; i++) {
-    if (array[i] === item) {
-      array.splice(i, 1);
-      i--;
-    }
-  }
-  return array;
-}
-function generateKey(length, characters) {
-  let result = "";
-  for (let i = 0; i < length; i++) {
-    result += characters.charAt(Math.floor(Math.random() * characters.length));
-  }
-  return result;
-}
-function removeChars(str, chars) {
-  let result = "";
+function solution(str) {
+  const alphabet = "abcdefghijklmnopqrstuvwxyz";
+  const charCount = new Array(26).fill(false);
+
   for (let i = 0; i < str.length; i++) {
-    if (chars.indexOf(str[i]) === -1) {
-      result += str[i];
+    const char = str.charAt(i).toLowerCase();
+    const index = alphabet.indexOf(char);
+    if (index !== -1) {
+      charCount[index] = true;
     }
   }
-  return result;
-}
-function sum(num) {
-  let total = num;
-  function add(nextNum) {
-    total += nextNum;
-    return add;
-  }
-  add.toString = function () {
-    return total;
-  };
-  return add;
+
+  return charCount.every((c) => c);
 }
 
-console.log(sum(3));
-console.log(sum(5)(3));
-console.log(sum(20)(5)(3));
+//________________________________________________________________________________________________________________________________________________
+
+function solution(sentence) {
+  const words = sentence.split(" ");
+
+  const averageLength =
+    words.reduce((acc, cur) => acc + cur.length, 0) / words.length;
+
+  const result = words.filter((word) => word.length > averageLength);
+
+  return result;
+}
+
+console.log(solution("This is a sample string"));
+console.log(solution("Some another sample"));
+console.log(solution("Do, do, do, do... it!"));
+
+//________________________________________________________________________________________________________________________________________________
+
+function Database() {
+  this.users = [];
+  this.adminEmail = "admin@example.com";
+  this.password = "password123";
+}
+
+Database.prototype.addUser = function (user) {
+  this.users.push(user);
+};
+
+function User(name, email) {
+  this.name = name;
+  this.email = email;
+}
+
+function VerifiedUser(name, email) {
+  User.call(this, name, email);
+  this.verified = true;
+}
+
+VerifiedUser.prototype = Object.create(User.prototype);
+
+function Guest(name, email) {
+  User.call(this, name, email);
+}
+
+Guest.prototype = Object.create(User.prototype);
+
+const db = new Database();
+
+const admin = new VerifiedUser("Admin", db.adminEmail);
+const user1 = new VerifiedUser("John", "john@example.com");
+const user2 = new Guest("Kate", "kate@example.com");
+
+if (admin.password === db.password) {
+  db.addUser(admin);
+}
+
+db.addUser(user1);
+db.addUser(user2);
+
+console.log(db.users);
