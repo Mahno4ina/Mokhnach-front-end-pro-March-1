8871519ca38dc5e40ce8d8c955219cc40442ba41
Daniel <118555923+Mahno4ina@users.noreply.github.com>
2023-05-24 14:11:42 +0300

    Update second.js

--- second.js
+++ second.js
@@ -1,16 +1,40 @@
-fetch('https://swapi.dev/api/people/')
-  .then(response => response.json())
-  .then(data => {
-    const characters = data.results;
-    const charactersWithFilms = characters.map(character => {
-      return {
-        name: character.name,
-        mass: character.mass,
-        height: character.height,
-        gender: character.gender,
-        films: character.films
-      }
-    });
-    const charactersWithThreeOrMoreFilms = charactersWithFilms.filter(character => character.films.length >= 3);
-    console.log(charactersWithThreeOrMoreFilms);
-  });
+-function removeElement(array, item) {
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === item) {
+      array.splice(i, 1);
+      i--;
+    }
+  }
+  return array;
+}
+function generateKey(length, characters) {
+  let result = "";
+  for (let i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * characters.length));
+  }
+  return result;
+}
+function removeChars(str, chars) {
+  let result = "";
+  for (let i = 0; i < str.length; i++) {
+    if (chars.indexOf(str[i]) === -1) {
+      result += str[i];
+    }
+  }
+  return result;
+}
+function sum(num) {
+  let total = num;
+  function add(nextNum) {
+    total += nextNum;
+    return add;
+  }
+  add.toString = function () {
+    return total;
+  };
+  return add;
+}
+
+console.log(sum(3));
+console.log(sum(5)(3));
+console.log(sum(20)(5)(3));
