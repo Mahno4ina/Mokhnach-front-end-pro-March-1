c3869847857dedf7480df6b1b724b5bfeaf9bb4e
Mahno4ina <patoraa4@gmail.com>
2023-05-24 14:18:21 +0300

    fd
    asd

--- second.js
+++ second.js
@@ -1,16 +1,44 @@
-fetch('https://swapi.dev/api/people/')
-  .then(response => response.json())
-  .then(data => {
-    const characters = data.results;
-    const charactersWithFilms = characters.map(character => {
-      return {
-        name: character.name,
-        mass: character.mass,
-        height: character.height,
-        gender: character.gender,
-        films: character.films
-      }
-    });
-    const charactersWithThreeOrMoreFilms = charactersWithFilms.filter(character => character.films.length >= 3);
-    console.log(charactersWithThreeOrMoreFilms);
+const options = [
+  { emoji: "😃", count: 0 },
+  { emoji: "😊", count: 0 },
+  { emoji: "🙂", count: 0 },
+  { emoji: "😐", count: 0 },
+  { emoji: "😞", count: 0 },
+];
+
+function displayResults() {
+  const resultsContainer = document.getElementById("results-container");
+  resultsContainer.innerHTML = "";
+  options.forEach((option) => {
+    const emojiElement = document.createElement("span");
+    emojiElement.innerHTML = option.emoji;
+    emojiElement.classList.add("emoji");
+
+    const countElement = document.createElement("span");
+    countElement.innerHTML = option.count;
+    countElement.classList.add("count");
+
+    const optionContainer = document.createElement("div");
+    optionContainer.appendChild(emojiElement);
+    optionContainer.appendChild(countElement);
+
+    resultsContainer.appendChild(optionContainer);
   });
+}
+
+function handleEmojiClick(index) {
+  options[index].count++;
+  displayResults();
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  displayResults();
+
+  const emojis = document.getElementsByClassName("emoji");
+
+  for (let i = 0; i < emojis.length; i++) {
+    emojis[i].addEventListener("click", function () {
+      handleEmojiClick(i);
+    });
+  }
+});
